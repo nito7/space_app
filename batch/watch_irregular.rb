45180794f49da73cf91084ee1605238e950cedd5
nito7 <naoki.itoh.7kusa@gmail.com>
2015-04-11 22:36:35 +0900

    update

--- batch/watch_irregular.rb
+++ batch/watch_irregular.rb
@@ -3,13 +3,19 @@
 
 require 'mysql'
 
+threshold  = 3
 connection = Mysql::new("127.0.0.1", "root", "root", "space_app")
 
-statement = connection.prepare("SELECT * FROM lightcurves ORDER BY mjd DESC");
+statement  = connection.prepare("SELECT * FROM lightcurves ORDER BY mjd DESC");
 begin
   result = statement.execute()
+
   result.each do |tuple|
-    puts tuple[0]
+    mjd  = tuple[0]
+    flux = tuple[1]
+    err  = tuple[2]
+
+    
   end
 ensure
   statement.close
